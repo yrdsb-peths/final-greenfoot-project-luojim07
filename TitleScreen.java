7dc410b18e06dc355697c886f3d311af269a27c3
luojim07 <348758226@gapps.yrdsb.ca>
2023-12-14 13:27:38 -0500

    Main bg

--- TitleScreen.java
+++ TitleScreen.java
@@ -19,6 +19,7 @@ public class TitleScreen extends World
         super(600, 400, 1);
         prepare();
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
@@ -28,5 +29,18 @@ public class TitleScreen extends World
         Witch witch = new Witch();
         addObject(witch,370,280);
 
+        Label label = new Label("Anesthesia", 110);
+        addObject(label,270,105);
+        
+        
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(null))
+        {
+            GameWorld world = new GameWorld();
+            Greenfoot.setWorld(world);
+        }
     }
 }
