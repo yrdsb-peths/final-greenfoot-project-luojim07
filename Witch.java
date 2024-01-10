4e8f11c7cf807b7468ffad2557b6cd4fcd50e928
luojim07 <348758226@gapps.yrdsb.ca>
2024-01-10 13:22:58 -0500

    movenment

--- Witch.java
+++ Witch.java
@@ -12,33 +12,64 @@ public class Witch extends Actor
      * Act - do whatever the Witch wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    GreenfootImage[] img = new GreenfootImage[4];
+    GreenfootImage[] idle = new GreenfootImage[4];
+    GreenfootImage[] left = new GreenfootImage[4];
+    GreenfootImage[] right = new GreenfootImage[4];
+    GreenfootImage[] up = new GreenfootImage[4];
+    GreenfootImage[] down = new GreenfootImage[4];
+
+    String facing = "right";
     SimpleTimer animationTimer = new SimpleTimer();
     public Witch()
     {
-        for (int i = 0; i < img.length; i++)
+        for (int i = 0; i < idle.length; i++)
         {
-            img[i] = new GreenfootImage("0"+ i + "_lown" + ".png");
-            setImage(img[i]);   
+            idle[i] = new GreenfootImage("0"+ i + "_lown" + ".png");
+            
         }
+        
+        for (int i = 0; i < left.length; i++)
+        {
+            left[i] = new GreenfootImage("0"+ (i+4) + "_lown" + ".png"); 
+        }
+        
+        animationTimer.mark();
+        setImage(idle[0]);
     }
     
      public void act()
     {
+        if(Greenfoot.isKeyDown("left"))
+        {
+            facing = "left";
+            move(-2);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            facing = "right";
+        }
+        
         animateWitch();
     }
     int imageIndex = 0;
     public void animateWitch()
     {
-        if (animationTimer.millisElapsed() < 300)
+        if (animationTimer.millisElapsed() < 200)
         {
             return;
         }
-
-
-        setImage(img[imageIndex]);
-        imageIndex = ((imageIndex + 1) % img.length);
         animationTimer.mark();
+        if(facing.equals("left"))
+        {
+            setImage(left[imageIndex]);  
+            imageIndex = (imageIndex + 1) % left.length;
+        }
+        else
+        {
+            setImage(idle[imageIndex]);  
+            imageIndex = (imageIndex + 1) % idle.length;
+        }
+
     }
 }
 
