632a1b62c398d8e3496af211146405e53af67f40
luojim07 <150279157+luojim07@users.noreply.github.com>
2024-01-12 13:23:09 -0500

    gamoverscreen

--- OutOfBounds.java
+++ OutOfBounds.java
@@ -17,5 +17,11 @@ public class OutOfBounds extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        prepare();
+    }
+    
+    private void prepare()
+    {
+        
     }
 }
