26cc30736ffb71d9e5d7303bf62110b7b4e32447
luojim07 <150279157+luojim07@users.noreply.github.com>
2024-01-15 12:41:54 -0500

    Update GameWorld.java

--- GameWorld.java
+++ GameWorld.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class GameWorld here.
  * 
- * @author (your name) 
+ * @author Jim Luo
  * @version (a version number or a date)
  */
 public class GameWorld extends World
